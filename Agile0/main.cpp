05de8e081f580f9aa97ad704a62f9d809474f08b
Kyler Geesink <32684761+tootgod@users.noreply.github.com>
2020-08-24 10:16:17 -0700

    Creates Agile0 Folder and main.cpp
    It also adds my part of the project and types it out

---
+++ Agile0/main.cpp
@@ -0,0 +1,15 @@
+using namespace std;
+#include <iostream>
+
+void nameKyler();
+
+int main()
+{
+	nameKyler();
+	return 0;
+}
+
+void nameKyler()
+{
+	cout << "Kyler Geesink" << endl;
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CS1C-Group8
+
+Please keep each project in a different folder. It will make it easier to keep track later
