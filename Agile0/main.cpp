c85f90d272d9075116836531b93d45559fe1551c
Shyormu <69994087+Shyormu@users.noreply.github.com>
2020-08-24 11:03:18 -0700

    Update main.cpp
    added Nicholas Lozano's name function

--- Agile0/main.cpp
+++ Agile0/main.cpp
@@ -2,14 +2,21 @@ using namespace std;
 #include <iostream>
 
 void nameKyler();
+void nameLozano();
 
 int main()
 {
 	nameKyler();
+	nameLozano();
 	return 0;
 }
 
 void nameKyler()
 {
 	cout << "Kyler Geesink" << endl;
+}
+
+void nameLozano()
+{
+	cout << "Nicholas Lozano" << endl;
 }
